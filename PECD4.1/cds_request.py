b79bbe04c6bcbbd5e639e886ea026dad66b07c59
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2025-05-29 10:28:08 +0200

    Add files

--- PECD4.1/cds_request.py
+++ PECD4.1/cds_request.py
@@ -10,17 +10,6 @@ variables = [
                 "surface_solar_radiation_downwards",
                 "total_precipitation",
                 "solar_generation_capacity_factor",
-                "latitude_weights",
-                "nuts_0_regions_mask",
-                "nuts_2_regions_mask",
-                "peof_regions_mask",
-                "peon_regions_mask",
-                "population_density_mask",
-                "power_law_coefficients",
-                "solar_pv_mask",
-                "szof_regions_mask",
-                "szon_regions_mask",
-                "wind_power_regions_mask"
             ]
 
 models = [
@@ -69,3 +58,131 @@ for var in variables:
                         }
                 client = cdsapi.Client()
                 client.retrieve(dataset, request, fname)
+
+
+# Metadata
+# Retrieved manually
+
+# Wind power data
+# ONSHORE
+var = "wind_power_onshore_capacity_factor"
+
+# Create folder if does not already exist
+Path(var).mkdir(parents=True, exist_ok=True)
+# Download historical data
+Path(var+"/historical/").mkdir(parents=True, exist_ok=True)
+for yr in np.arange(1980,2021):
+    fname=var+"/historical/"+var+'_'+str(yr)+'.zip'
+
+    dataset = "sis-energy-pecd"
+    request = {
+        "pecd_version": "pecd4_1",
+        "temporal_period": ["historical"],
+        "origin": ["era5_reanalysis"],
+        "year": [str(yr)],
+        "variable": [var],
+        "technology": [
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39"
+        ],
+        "spatial_resolution": ["peon"]
+    }
+
+    client = cdsapi.Client()
+    client.retrieve(dataset, request, fname)
+    
+# Download future data
+Path(var+"/future/").mkdir(parents=True, exist_ok=True)
+for model in models:
+    Path(var+"/future/"+model).mkdir(parents=True, exist_ok=True)
+    for yr in np.arange(2015, 2066):
+        fname = var+"/future/"+model+'/'+var+'_'+str(yr)+'_'+model+'.zip'
+
+        dataset = "sis-energy-pecd"
+        request = {
+            "pecd_version": "pecd4_1",
+            "temporal_period": ["future_projections"],
+            "origin": [model],
+            "emissions": ["ssp2_4_5"],
+            "variable": [var],
+            "technology": [
+                "30",
+                "31",
+                "32",
+                "33",
+                "34",
+                "35",
+                "36",
+                "37",
+                "38",
+                "39"
+            ],
+            "spatial_resolution": ["peon"],
+            "year": [str(yr)]
+        }
+        
+        client = cdsapi.Client()
+        client.retrieve(dataset, request, fname)
+
+# OFFSHORE
+var = "wind_power_offshore_capacity_factor"
+
+# Create folder if does not already exist
+Path(var).mkdir(parents=True, exist_ok=True)
+# Download historical data
+Path(var+"/historical/").mkdir(parents=True, exist_ok=True)
+for yr in np.arange(1980,2021):
+    fname=var+"/historical/"+var+'_'+str(yr)+'.zip'
+
+    dataset = "sis-energy-pecd"
+    request = {
+        "pecd_version": "pecd4_1",
+        "temporal_period": ["historical"],
+        "origin": ["era5_reanalysis"],
+        "variable": [var],
+        "technology": [
+            "20",
+            "21",
+            "22"
+        ],
+        "spatial_resolution": ["peof"],
+        "year": [str(yr)]
+    }
+
+    client = cdsapi.Client()
+    client.retrieve(dataset, request, fname)
+
+# Download future data
+Path(var+"/future/").mkdir(parents=True, exist_ok=True)
+for model in models:
+    Path(var+"/future/"+model).mkdir(parents=True, exist_ok=True)
+    for yr in np.arange(2015, 2066):
+        fname = var+"/future/"+model+'/'+var+'_'+str(yr)+'_'+model+'.zip'
+        
+        dataset = "sis-energy-pecd"
+        request = {
+            "pecd_version": "pecd4_1",
+            "temporal_period": ["future_projections"],
+            "origin": [model],
+            "emissions": ["ssp2_4_5"],
+            "variable": [var],
+            "technology": [
+                "20",
+                "21",
+                "22"
+            ],
+            "spatial_resolution": ["peof"],
+            "year": [str(yr)]
+        }
+        
+        client = cdsapi.Client()
+        client.retrieve(dataset, request, fname)
+    

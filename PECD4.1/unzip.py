a753d36dcde9e0c417968c7fb612b42148c08679
ClimatClement <contact@climatclement.com>
2025-06-05 16:36:58 +0200

    Adding config.py with local path. Unziping PECD4.1 data

---
+++ PECD4.1/unzip.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+import zipfile
+from pathlib import Path
+from config import BDD_PATH, TRASH_PATH
+
+os.makedirs(TRASH_PATH, exist_ok=True)
+
+files_to_unzip=[f for f in Path().glob('**/*.zip')]
+
+for input_file in files_to_unzip:
+
+    output_path = BDD_PATH.joinpath(input_file.parent)
+    os.makedirs(output_path, exist_ok=True)
+
+    print(f'Unzipping file {input_file} to {output_path}')
+    
+    with zipfile.ZipFile(input_file, 'r') as zip_ref:
+        zip_ref.extractall(output_path)
+
+#    shutil.move(input_file, TRASH_PATH)
---
+++ config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+#LOCAL PATHS
+BDD_PATH = Path('/media/cl-ment-devenet/Partage/bdd')
+
+OUTPUT_DATA_PATH = Path('/media/cl-ment-devenet/Partage/bdd/Produits')
+
+TRASH_PATH = Path('/media/cl-ment-devenet/Partage/bdd/trash')

a13382095af494f9da4ea837d8c7389410dfdc19
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2025-06-23 19:55:06 +0100

    Add identify_problematic_events

--- weavepy/events.py
+++ weavepy/events.py
@@ -1,5 +1,29 @@
 import pandas as pd
 
+def identify_problematic_events(climate_daily, energy_daily, climate_variable_comparison = '<', climate_variable_threshold = 0,
+                                energy_variable_comparison = '<', energy_variable_threshold = 0.5, ):
+    # Climate condition
+    if climate_variable_comparison == '<':
+        condition1 = climate_daily < climate_variable_threshold
+    elif climate_variable_comparison == '<=':
+        condition1 = climate_daily <= climate_variable_threshold
+    elif climate_variable_comparison == '>':
+        condition1 = climate_daily > climate_variable_threshold
+    elif climate_variable_comparison == '>=':
+        condition1 = climate_daily >= climate_variable_threshold
+
+    # Energy condition
+    if energy_variable_comparison == '<':
+        condition2 = energy_daily < energy_variable_threshold
+    elif energy_variable_comparison == '<=':
+        condition2 = energy_daily <= energy_variable_threshold
+    elif energy_variable_comparison == '>':
+        condition2 = energy_daily > energy_variable_threshold
+    elif energy_variable_comparison == '>=':
+        condition2 = energy_daily >= energy_variable_threshold
+    return condition1 & condition2
+                                
+
 def find_events(series):
     events = []
     event_ongoing = False

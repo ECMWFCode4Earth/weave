334555be22848e6cd2da20d9319acecbd5eeb003
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2025-06-23 19:55:06 +0100

    Add identify_problematic_events

--- weavepy/__init__.py
+++ weavepy/__init__.py
@@ -11,4 +11,5 @@ import seaborn as sns
 
 # Specific functions
 from .load import get_data, explore_database
-from .events import find_events
+from .events import find_events, identify_problematic_events
+from .plot import event_stripplot, nb_events_barplot, event_duration_histplot, event_seasonnality_histplot

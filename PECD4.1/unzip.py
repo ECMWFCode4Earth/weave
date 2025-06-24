ce1df8d43fe344d6ae45faa1d2e9923e8403234e
ClimatClement <contact@climatclement.com>
2025-06-24 16:32:09 +0200

    Correct unzip.py path management

--- PECD4.1/unzip.py
+++ PECD4.1/unzip.py
@@ -12,6 +12,8 @@ sys.path.append(config_dir)
 
 from config import BDD_PATH
 
+PECD_BDD_PATH = Path.joinpath(BDD_PATH, 'PECD4.1')
+
 if trash:
     from config import TRASH_PATH
     # Create trash folder
@@ -22,12 +24,12 @@ files_to_unzip=[f for f in Path().glob('**/*.zip')]
 for input_file in files_to_unzip:
 
     # Determine where file should be unzipped and create if necessary
-    output_path = BDD_PATH.joinpath(input_file.parent)
+    output_path = Path.joinpath(PECD_BDD_PATH,input_file.parent)
     os.makedirs(output_path, exist_ok=True)
 
     # Actually unzip the file (if it does not already exists)
     with zipfile.ZipFile(input_file, 'r') as zip_ref:
-        if not os.path.isfile(output_path):
+        if not os.path.isfile(Path.joinpath(output_path,zip_ref.namelist()[0])):
             print(f'Unzipping file {input_file} to {output_path}')
             zip_ref.extractall(output_path)
 

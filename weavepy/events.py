1fd799d3f4a2320d1c052e08bb78dd518ce57074
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2025-07-09 16:51:49 +0200

    New events identification method outputing a pandas dataframe

--- weavepy/events.py
+++ weavepy/events.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def identify_events(var_daily, var_comparison, var_threshold):
+def identify_pb_days(var_daily, var_comparison, var_threshold):
     # Climate condition
     if var_comparison == '<':
         return var_daily < var_threshold
@@ -11,16 +11,25 @@ def identify_events(var_daily, var_comparison, var_threshold):
     elif var_comparison == '>=':
         return var_daily >= var_threshold                                
 
-def find_events(series):
-    events = []
+def identify_events(series, model = "", scenario = "", country = ""):
+    events = pd.DataFrame()
+    columns = ["model", "scenario", "country", "eventID", "start", "end", "duration"]
     event_ongoing = False
-    for line in pd.DataFrame(series.rename("problem")).itertuples():
-        if (line.problem == True) :
-            if not event_ongoing:
-                events.append([line.Index]) # Create new event
+    eventID = 0
+    for d in series.date.values:
+        pb = (series.sel(date = d) == True)
+        d = pd.to_datetime(d)
+        if pb:
+            if not event_ongoing: # New event
                 event_ongoing = True
-            elif event_ongoing:
-                events[-1].append(line.Index) # Continue previous event
+                start = d
+                end = d
+            elif event_ongoing: # Event in continuing
+                end = d
         else:
-            event_ongoing = False
-    return events
+            if event_ongoing:
+                event_ongoing = False
+                event = pd.DataFrame([[model, scenario, country, eventID, start, end, d-start]], columns = columns)
+                events = pd.concat([events, event])
+                eventID += 1
+    return events.assign(year = events.start.dt.year)

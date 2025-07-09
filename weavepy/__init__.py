d2be1acf67c61dcc58b8994d82c225ed96c07af7
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2025-07-09 16:52:05 +0200

    Define palettes

--- weavepy/__init__.py
+++ weavepy/__init__.py
@@ -14,7 +14,7 @@ from tqdm import tqdm
 
 # Specific functions
 from .load import get_data, explore_database, load_vars
-from .events import find_events, identify_events
+from .events import identify_pb_days, identify_events
 from .plot import event_stripplot, nb_events_barplot, event_duration_histplot, event_seasonnality_histplot
 from .utils import get_period_length, get_period_min_max
 
@@ -23,4 +23,23 @@ model_aliases = {
     "cmcc_cm2_sr5":"CMR5",
     "ec_earth3":"ECE3", 
     "mpi_esm1_2_hr":"MEHR", 
+}
+
+scenario_colors = {
+    "historical":'k',
+    "SP119": (0,173,207),
+    "SP126": (23,60,102),
+    "SP245": (247,148,32),
+    "SP370": (231,29,37),
+    "SP585": (149,27,30),
+}
+
+model_linestyles = {
+    "ERA5":'-',
+    "AWCM":'--',
+    "BCCS":'-.',
+    "CMR5":':',
+    "ECE3":'-..',
+    "MEHR":(0,(1,5)), # Densely dotted
+    "MRM2":(5,(10,3)), # long dashes
 }

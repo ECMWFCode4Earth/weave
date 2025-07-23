9a8a70f0578203170946469c94ff3cf82b62515b
ClimatClement <contact@climatclement.com>
2025-07-23 13:25:45 +0200

    Stop tracking config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-#LOCAL PATHS
-BDD_PATH = Path('/Users/bourdin/Library/CloudStorage/OneDrive-Nexus365/WEAVE/weave/')
-
-OUTPUT_DATA_PATH = Path('/Users/bourdin/Library/CloudStorage/OneDrive-Nexus365/WEAVE/weave/PECD4.1/')
-
-#TRASH_PATH = Path('/media/cl-ment-devenet/Partage/bdd/trash')

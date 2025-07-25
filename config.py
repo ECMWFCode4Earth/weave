bcf383736a341a2fba66d92e33f80b8f3afb9d54
Stella Bourdin <stella.bourdin@physics.ox.ac.uk>
2025-07-25 18:16:32 +0200

    Update with new path for spirit

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ if host.startswith("spirit"):
     #LOCAL PATHS
     BDD_PATH = Path('/modfs/project/')
     OUTPUT_DATA_PATH = Path('/modfs/project/')
+    CACHE_DATA_PATH = Path('/data/sbourdin/PECD42_cache_data/')
+    TRASH_PATH = Path('/scratchu/sbourdin/PECD42_trash')

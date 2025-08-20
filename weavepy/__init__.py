23ce3d3fe143b00c75c9c2c1c550479b4883e183
Stella Bourdin <stella.bourdin@physics.ox.ac.uk>
2025-08-20 13:06:26 +0200

    Update with new plot functions

--- weavepy/__init__.py
+++ weavepy/__init__.py
@@ -16,7 +16,7 @@ from tqdm import tqdm
 # Specific functions
 from .load import get_data, load_vars
 from .events import identify_pb_days, identify_events_whole_base
-from .plot import event_stripplot, nb_events_barplot, event_duration_histplot, event_seasonnality_histplot
+from .plot import scenario_colors, model_linestyles, nb_event_timeseries, event_duration_hist, event_seasonality_kde
 from .utils import get_period_length, get_period_min_max
 
 model_aliases = {
@@ -26,27 +26,3 @@ model_aliases = {
     "mpi_esm1_2_hr":"MEHR", 
 }
 
-scenario_colors = {
-    "historical":'k',
-    "SP119": np.array([0,173,207])/256,
-    "SP126": np.array([23,60,102])/256,
-    "SP245": np.array([247,148,32])/256,
-    "SP370": np.array([231,29,37])/256,
-    "SP585": np.array([149,27,30])/256,
-}
-
-model_linestyles = {
-    "ERA5":'-',
-    "AWCM":'--',
-    "BCCS":'-.',
-    "CMR5":':',
-    "ECE3":(0, (3, 1, 1, 1, 1, 1)),
-    "MEHR":(0,(1,5)), # Densely dotted
-    "MRM2":(5,(10,3)), # long dashes
-    'AWI-_AWCM':'--',
-    'BCC-_BCCS':'-.',
-    "CMCC_CMR5":':',
-    "ECEC_ECE3":(0, (3, 1, 1, 1, 1, 1)), #densely dashdotdotted
-    'MPI-_MEHR':(0,(1,5)), # Densely dotted
-    'MRI-_MRM2':(5,(10,3)), # long dashes
-}

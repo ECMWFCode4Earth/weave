0a210e596c7bf5e4aed2e20c1b89acae12fa5b53
Stella Bourdin <stella.bourdin@physics.ox.ac.uk>
2025-08-20 13:05:46 +0200

    Update new plots

--- weavepy/plot.py
+++ weavepy/plot.py
@@ -2,7 +2,115 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import seaborn as sns
 import numpy as np
+import pandas as pd
 
+scenario_colors = {
+    "historical":'k',
+    "SP119": np.array([0,173,207])/256,
+    "SP126": np.array([23,60,102])/256,
+    "SP245": np.array([247,148,32])/256,
+    "SP370": np.array([231,29,37])/256,
+    "SP585": np.array([149,27,30])/256,
+}
+
+model_linestyles = {
+    "ERA5":'-',
+    "AWCM":'--',
+    "BCCS":'-.',
+    "CMR5":':',
+    "ECE3":(0, (3, 1, 1, 1, 1, 1)),
+    "MEHR":(0,(1,5)), # Densely dotted
+    "MRM2":(5,(10,3)), # long dashes
+    'AWI-_AWCM':'--',
+    'BCC-_BCCS':'-.',
+    "CMCC_CMR5":':',
+    "ECEC_ECE3":(0, (3, 1, 1, 1, 1, 1)), #densely dashdotdotted
+    'MPI-_MEHR':(0,(1,5)), # Densely dotted
+    'MRI-_MRM2':(5,(10,3)), # long dashes
+}
+
+def nb_event_timeseries(N_climate_events, N_energy_events, N_compound_events):
+    fig, axs = plt.subplots(3, figsize = [12, 8], sharex = True) 
+    Nrolls = {}
+    for i, N in enumerate([N_climate_events, N_energy_events, N_compound_events]):
+        for scenario in N_climate_events.scenario.unique():
+            Nrolls[str(scenario)] = {}
+            for model in N_climate_events.model.unique():
+                N2plot = N[(N.scenario == scenario) & (N.model == model)].set_index("year")
+                # Raw data
+                #N2plot.eventID.plot(ax = axs[i], alpha = 0.25, 
+                #                    c = scenario_colors[scenario], linestyle = model_linestyles[model])
+                #axs[i].plot(N2plot.year, N2plot.eventID, alpha = 0.5,
+                #            c = scenario_colors[scenario], linestyle = model_linestyles[model])
+                # Running mean
+                Nroll = N2plot.eventID.rolling(20, center = True).mean()
+                Nrolls[str(scenario)][str(model)] = Nroll
+                Nroll.plot(ax = axs[i], 
+                           c = scenario_colors[scenario], 
+                           linestyle = model_linestyles[model], 
+                          alpha = 0.5)
+            mm_mean = pd.concat(list(Nrolls[scenario].values())).rename(scenario).reset_index().groupby("year").mean()
+            mm_mean.plot(ax = axs[i], linewidth = 5, color = scenario_colors[scenario],)
+        axs[i].set_ylabel("#events")
+    axs[0].set_title("Climate events")
+    axs[1].set_title("Energy events")
+    axs[2].set_title("Compound events")
+    plt.suptitle("Evolution of the number of events")
+    plt.tight_layout()
+
+def event_duration_hist(climate_events, energy_events, compound_events):
+    fig, axs = plt.subplots(3, figsize = [10, 10], sharex = True) # TODO: Rolling mean
+    for i, e in enumerate([climate_events, energy_events, compound_events]):
+        e["duration_days"] = e["duration"].astype(int) * 1e-9 / 3600 / 24
+    
+        # Scenarios
+        for scenario in e.scenario.unique():
+            e2plot = e[(e.scenario == scenario)].set_index("year")
+            sns.histplot(data = e2plot, x = "duration_days", ax = axs[i], 
+            color = scenario_colors[scenario], element = "step", alpha = 0.1,
+            bins = np.arange(0.5,30,1), stat = "proportion", linewidth = 2,
+            )
+            mean = e2plot.duration_days.mean()
+            axs[i].axvline(x = mean, color = scenario_colors[scenario], label = scenario)
+    axs[0].set_title("Climate events")
+    axs[1].set_title("Energy events")
+    axs[2].set_title("Compound events")
+    plt.suptitle("Duration of the events")
+    plt.legend()
+    plt.xlim(0,10)
+    plt.tight_layout()
+
+def vonmises_kde(data, kappa, n_bins=100):
+    from scipy.special import i0
+    bins = np.linspace(-np.pi, np.pi, n_bins)
+    x = np.linspace(-np.pi, np.pi, n_bins)
+    # integrate vonmises kernels
+    kde = np.exp(kappa*np.cos(x[:, None]-data[None, :])).sum(1)/(2*np.pi*i0(kappa))
+    kde /= np.trapezoid(kde, x=bins)
+    return bins, kde
+
+doy_first_day_month = [1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335]
+
+def event_seasonality_kde(climate_days, energy_days, compound_days):
+    fig, axs = plt.subplots(1, 3, subplot_kw = dict(projection = "polar"), figsize = (15,5))
+    for i, e in enumerate([climate_days, energy_days, compound_days]):
+        e["doy"] = e["time"].dt.dayofyear
+        for scenario in np.unique(e.scenario):
+            days_scenario = e.sel(scenario = scenario)#.set_index("year")
+            doys2plot = days_scenario.doy.where(days_scenario).values.flatten()
+            x, kde = vonmises_kde(doys2plot[~np.isnan(doys2plot)] * 2 * np.pi / 365, 180)
+            axs[i].plot(x, kde, color = scenario_colors[scenario], label = scenario)
+        axs[i].set_xticks(np.array(doy_first_day_month) * 2 * np.pi / 365, 'JFMAMJJASOND')
+        axs[i].set_yticks([])
+    axs[0].set_title("Climate events")
+    axs[1].set_title("Energy events")
+    axs[2].set_title("Compound events")
+    axs[2].legend(fontsize = "small", loc = "lower left")
+    plt.suptitle("Seasonality of the events")
+    
+    plt.tight_layout()
+
+    
 def event_stripplot(var_days, ax, index= "date", title = '', color = None, fill_between_kws = dict()):
     """
     bool_series (pd.Series): Boolean series where the days when the condition is met are True, and the others are false
